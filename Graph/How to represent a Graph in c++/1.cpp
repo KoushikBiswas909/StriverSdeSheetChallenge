0294af489d22f2df6e7062e563ec831906642d58
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2022-06-06 23:39:20 +0530

    Create 1.cpp

---
+++ Graph/How to represent a Graph in c++/1.cpp
@@ -0,0 +1,20 @@
+/* Undirected Unweighted Graph representation as Adjacency Matrix */
+
+
+int main(){
+	// n = num of Nodes/Vertex ,  m = num of Edges
+	int n,m;
+	cin>>n>>m;
+
+	// declare adjancent matrix of size N+1, if graph is 1 based indexing
+	// otherwise take N size
+	int adj[n+1][n+1];
+
+	// taking edges as input
+	for(int i=0;i<m;i++){
+		int u,v;
+		cin>>u>>v;
+		adj[u][v]=1;
+		adj[v][u]=1;
+	}
+}
