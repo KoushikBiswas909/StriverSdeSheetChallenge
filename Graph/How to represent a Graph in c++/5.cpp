e0110bf767d52fa8da8d2174866057e50deaa704
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2022-06-07 00:35:13 +0530

    Create 5.cpp

---
+++ Graph/How to represent a Graph in c++/5.cpp
@@ -0,0 +1,21 @@
+* Directed Weighted Graph representation as Adjacency List */
+
+int main(){
+	// n = num of Nodes/Vertex ,  m = num of Edges
+	int n,m;
+	cin>>n>>m;
+
+    //declare Adjancent List, a vector array of size n+1.
+	vector<pair<int,int>> adj[n+1];
+
+
+	// taking edges as input, wt is weight
+	for(int i=0;i<m;i++){
+		int u,v,wt;
+		cin>>u>>v>>wt;
+		
+
+		adj[u].push_back({v,wt}); //graph direction u to v.
+	}
+	return 0;
+}
