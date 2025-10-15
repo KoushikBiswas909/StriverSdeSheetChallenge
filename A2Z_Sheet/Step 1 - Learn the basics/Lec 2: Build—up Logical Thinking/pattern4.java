374da1fc06a5079ea6543edd72c424776ff9d377
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2025-10-15 23:41:08 +0530

    Add pattern4.java to print number patterns
    This program prints a pattern of numbers, where each line contains the line number repeated according to its value.

---
+++ A2Z_Sheet/Step 1 - Learn the basics/Lec 2: Build—up Logical Thinking/pattern4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+          System.out.print(i+1);
+        }
+        System.out.println();
+      }
+  }
+}
+
+/*
+Output:
+
+1
+22
+333
+4444
+55555
+*/
