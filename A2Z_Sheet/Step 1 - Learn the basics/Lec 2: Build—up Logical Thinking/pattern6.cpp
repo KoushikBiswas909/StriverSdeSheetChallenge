dbf8d0b84c4a26a9ece555119bddc3364b4be918
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2025-10-16 01:03:10 +0530

    Add pattern6.cpp for number pattern printing
    Implement a pattern printing program in C++ that outputs decreasing sequences of numbers.

---
+++ A2Z_Sheet/Step 1 - Learn the basics/Lec 2: Build—up Logical Thinking/pattern6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for(int i=0;i<5;i++){
+      for(int j=0;j<5-i;j++){
+        cout<<j+1;
+      }
+      cout<<endl;
+    }
+}
+
+
+/*
+Output:
+
+12345
+1234
+123
+12
+1
+
+*/
