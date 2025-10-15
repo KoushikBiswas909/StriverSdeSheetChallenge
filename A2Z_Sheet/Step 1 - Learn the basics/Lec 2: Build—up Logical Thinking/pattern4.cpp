91db66e96898720e20802b9fd2a90eafa0fdb2d7
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2025-10-15 23:40:27 +0530

    Add pattern4.cpp for number pattern printing
    Implement a pattern printing program that outputs a specific sequence of numbers based on the row index.

---
+++ A2Z_Sheet/Step 1 - Learn the basics/Lec 2: Build—up Logical Thinking/pattern4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for(int i=0;i<5;i++){
+      for(int j=0;j<i+1;j++){
+        cout<<i+1;
+      }
+      cout<<endl;
+    }
+}
+
+/*
+Output:
+1
+22
+333
+4444
+55555
+*/
