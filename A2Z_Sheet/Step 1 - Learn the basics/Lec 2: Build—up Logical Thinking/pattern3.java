226c06a7bcc2e90c272ca9b854bb075f0b42e9bc
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2025-10-15 23:38:45 +0530

    Add pattern printing program in pattern3.java
    This Java program prints a pattern of numbers in increasing order for each line.

---
+++ A2Z_Sheet/Step 1 - Learn the basics/Lec 2: Build—up Logical Thinking/pattern3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+          System.out.print(j+1);
+        }
+        System.out.println();
+      }
+  }
+}
+
+
+/*
+Output:
+
+1
+12
+123
+1234
+12345
+*/
